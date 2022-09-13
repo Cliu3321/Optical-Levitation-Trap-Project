bff2d8892ac93af3fe385405000024e6d054df4b
Cliu3321 <110605396+Cliu3321@users.noreply.github.com>
2022-09-13 10:40:17 +0100

    Update particle position_with PID control.py

--- particle position_with PID control.py
+++ particle position_with PID control.py
@@ -2,6 +2,14 @@
 """
 Created on Thu Aug 24 16:01:09 2022
 
+Measure the position of the optically trapped particle with and without the PID control
+
+Requires SPI Interface Enabled in Raspberry Pi Configuration
+ 
+Requires pre-instalation of SPI_Dev  https://github.com/doceme/py-spidev
+
+Requires ADCDACPi.py from ABElectronics
+
 @author: LCY
 """
 from __future__ import absolute_import, division, print_function, \
@@ -25,8 +33,23 @@ except ImportError:
     except ImportError:
         raise ImportError(
             "Failed to import library from parent folder")
-        
-def stats_read(zs_samples, samples): # Read N samples of voltage data, do indicative stats.
+
+
+def stats_read(zs_samples, samples):
+	'''
+	Read from ADC and measure the position of the particle
+	
+Args:
+        zs_samples: number of samples when PID is turned off
+        samples: number of samples when PID is turned on
+Returns: 
+	time: An array of time data
+	position_data: an array contains the position of the particle corresponding to each time
+	zs: set point
+	u_data: A array of PID controller signal
+	
+	'''
+.
 
     adcdac = ADCDACPi(1)     # create instance of ADCDAC Pi with DAC gain of 1
     
@@ -115,6 +138,8 @@ def stats_read(zs_samples, samples): # Read N samples of voltage data, do indica
         adcdac.set_dac_voltage(1,u)
         u_data.append(u)
         i = i+1
+	
+#Take more data with PID turned off
         
     while i <= samples + zs_samples - 1:     # Populate data array
         u_bias = 0.5
@@ -158,21 +183,40 @@ def plot_data_time(time, data):
     plt.ylabel("Particle Position")
     plt.title("Particle Postion vs Time")
     plt.show()
-     
+
+   
 def variance(x, zs_samples,samples):
+'''
+Calculate the vairance of particle's position with and without PID  
+
+Args:
+        x: a array of particle's position
+	zs_samples: number of samples when PID is turned off
+        samples: number of samples when PID is turned on
+Returns: 
+	variance1: The variance of the position of the particle without PID
+	variance2: The variance of the position of the particle with PID
+	
+'''
     data1 = x[:zs_samples]
     data2 = x[zs_samples:(zs_samples+samples)]
     
     mean1 = np.mean(data1)
-    variance1 = np.sum((data1 - mean1)**2)/ len(data1) 
+    variance1 = np.sum((data1 - mean1)**2)/ len(data1) # position data without PID
     
     mean2 = np.mean(data2)
-    variance2 = np.sum((data2 - mean2)**2)/ len(data2)
-    print(variance2-variance1, variance2/variance1)
+    variance2 = np.sum((data2 - mean2)**2)/ len(data2) # position data with PID
+    print('variance2 -  variance 1 = ', variance2-variance1)
+    print('variance2 / variance 1 = ', variance2/variance1)
     
     return variance1, variance2
 
+# Plot the position of the particle
 def plot_difference(time,data,zs):
+'''
+	Plot the position of the particle relative to the set point
+	
+'''
     diff = data - zs
     plt.figure(2)
     params = {	
@@ -189,8 +233,13 @@ def plot_difference(time,data,zs):
     plt.ylabel("Difference")
     plt.title("Deviation from the setpoint")
     plt.show()
-    
+
+# Plot the 
 def plot_pid(time,u):
+'''
+Plot the PID controller signal as a function of time
+	
+'''
     plt.figure(3)
     params = {	
 	 "axes.labelsize": 16,
@@ -208,6 +257,10 @@ def plot_pid(time,u):
     
     
 def plot_pidandpos(time,u,pos):
+'''
+Plot the PID controller signal and the position of the particle as a function of time
+	
+'''
     plt.figure(3)
     params = {	
 	 "axes.labelsize": 16,
@@ -224,8 +277,8 @@ def plot_pidandpos(time,u,pos):
     plt.ylabel("u")
     plt.show()
 
-zs_samples = 10000
-samples = 10000
+zs_samples = 10000 # number of samples when PID is turned off
+samples = 10000 # number of samples when PID is turned on 
 time,position_data,zs ,u_data = stats_read(zs_samples, samples)
 plot_data_time(time, position_data)
 plot_difference(time,position_data,zs)
